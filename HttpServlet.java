78f99988a24e5f46a77f4b4826f53c39632d290d
Abdulmajid Ramadan Hassan <45086070+Sudan1841@users.noreply.github.com>
2023-03-28 16:36:44 -0600

    Add files via upload

---
+++ HttpServlet.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Hello;
+
+/**
+ * @author Tiger
+ *
+ */
+public class HttpServlet {
+
+	/**
+	 * 
+	 */
+	public HttpServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
